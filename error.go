6f927e896d7a1ee84bdb58c6631057c6a10040a3
CypriotUnknown <49270120+CypriotUnknown@users.noreply.github.com>
2024-03-21 18:50:23 +0200

    added panic to JSONError

--- error.go
+++ error.go
@@ -1,25 +1,31 @@
 package utilities
 
-type JsonError struct {
+import "log"
+
+type jsonError struct {
 	Err     error  `json:"err"`
 	Message string `json:"message"`
 }
 
-func JSONError(err error, message string) JsonError {
-	return JsonError{
+func JSONError(err error, message string) jsonError {
+	return jsonError{
 		Err:     err,
 		Message: message,
 	}
 }
 
-func (e JsonError) Error() string {
+func (e *jsonError) Panic() {
+	log.Panicln(e.Error())
+}
+
+func (e jsonError) Error() string {
 	return ToJSONString(e)
 }
 
-func (e JsonError) ToJSON() (jsonBytes []byte) {
+func (e jsonError) ToJSON() (jsonBytes []byte) {
 	return ToJSON(e)
 }
 
-func (e JsonError) ToJSONString() (jsonString string) {
+func (e jsonError) ToJSONString() (jsonString string) {
 	return ToJSONString(e)
 }
