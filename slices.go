05116e5d1c67780e01c0b1d0f3370bba1fd11254
CypriotUnknown <49270120+CypriotUnknown@users.noreply.github.com>
2024-03-26 01:03:04 +0200

    changed findinslice to return index

--- slices.go
+++ slices.go
@@ -11,39 +11,13 @@ func RemoveFromSlice[X any](slice []X, index int) []X {
 	return append(slice[:index], slice[index+1:]...)
 }
 
-func FindInSlice2[X any](slice []X, condition func(object X) bool) int {
-	// Iterate over the slice
+func FindInSlice[X any](slice []X, condition func(object X) bool) int {
 	for i, value := range slice {
-		// If the current element matches the target value, return its index
 		if condition(value) {
 			return i
 		}
 	}
-	// If the target value is not found, return -1
 	return -1
-}
-
-func FindInSlice[X any, T []X](slice *T, condition func(object *X) bool) (*X, *int) {
-
-	var returnObject X
-	var index int
-
-	success := false
-
-	for i, obj := range *slice {
-		if condition(&obj) {
-			returnObject = obj
-			success = true
-			index = i
-			break
-		}
-	}
-
-	if success {
-		return &returnObject, &index
-	} else {
-		return nil, nil
-	}
 }
 
 func MapSlice[X any, R any](array []X, mapFunction func(object X, index int) R) []R {
